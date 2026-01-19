2256a0c4353d8e9e8be4e793698c934e3ddd0f7e
shiro <okano.03.remi@gmail.com>
2026-01-19 14:49:35 +0900

    コントローラ教材コピペ

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,30 +9,42 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-  public function index(){
+  public function index()
+  {
     $todos = Todo::with('category')->get();
     $categories = Category::all();
+
     return view('index', compact('todos', 'categories'));
-}
-  public function store(TodoRequest $request){
+  }
+
+  public function search(Request $request)
+  {
+    $todos = Todo::with('category')->categorySearch($request->category_id)->keywordSearch($request->keyword)->get();
+    $categories = Category::all();
+
+    return view('index', compact('todos', 'categories'));
+  }
+
+  public function store(TodoRequest $request)
+  {
     $todo = $request->only(['category_id', 'content']);
     Todo::create($todo);
+
     return redirect('/')->with('message', 'Todoを作成しました');
-}
-  public function update(TodoRequest $request){
-    $todo=$request->only(['content']);
+  }
+
+  public function update(TodoRequest $request)
+  {
+    $todo = $request->only(['content']);
     Todo::find($request->id)->update($todo);
+
     return redirect('/')->with('message', 'Todoを更新しました');
-}
-  public function destroy(Request $request){
-    Todo::find($request->id)->delete();
+  }
+
+  public function destroy(Request $request)
+  {
+    Todo::find($request->todo_id)->delete();
+
     return redirect('/')->with('message', 'Todoを削除しました');
-}
-  public function search(Request $request){
-  $todos = Todo::with('category')->CategorySearch($request->category_id)->KeywordSearch($request->keyword)->get();
-  $categories = Category::all();
-
-  return view('index', compact('todos', 'categories'));
-}
-
+  }
 }
